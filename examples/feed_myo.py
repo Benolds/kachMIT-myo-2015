249c8e2fd5f1844fd2d4e76f475b4a7351e12f11
Benjamin Reynolds <reynoldsben94@gmail.com>
2015-09-20 05:20:04 -0400

    Sends correct data for left and right hands to SC

--- examples/feed_myo.py
+++ examples/feed_myo.py
@@ -57,24 +57,27 @@ def main():
         # myo = feed.wait_for_single_device(2)
         # if not myo:
         time.sleep(1)
-        myos_unsorted = feed.get_connected_devices()
         myos = []
-        if len(myos) == 0:
+        myos_unsorted = feed.get_connected_devices()
+        if len(myos_unsorted) == 0:
             print("No Myo connected after 1 second.")
             return
 
         print("Hello, Myo!")
-        print("Found " + str(len(myos)) + " myos")
+        print("Found " + str(len(myos_unsorted)) + " myos")
         for myo in myos_unsorted:
-            print("Found myo for " + str(myo.arm) + " arm")
-            if myo.arm == 'right': # right
-                print("Right arm")
-                myos.append(myo)
-        for myo in myos_unsorted:
-            print("Found myo for " + str(myo.arm) + " arm")
-            if myo.arm == 'left': # right
-                print("Right arm")
-                myos.append(myo)
+            myos.append(myo)
+        #     print("Found myo for " + str(myo.arm) + " arm")
+        #     if myo.arm == 'right': # right
+        #         print("Right arm")
+        #         myos.append(myo)
+        # for myo in myos_unsorted:
+        #     print("Found myo for " + str(myo.arm) + " arm")
+        #     if myo.arm == 'left': # right
+        #         print("Right arm")
+        #         myos.append(myo)
+
+
         
             # elif myo.arm == 'left': #left
             #     print("Left arm")
@@ -101,7 +104,10 @@ def main():
             msg_data = []
 
             for myo in myos:
-                arm_string = 'right' if myo.arm == 'right' else 'left'
+                
+                #arm_string = 'right' if myo.arm == 'right' else 'left'
+                arm_string = 'right' if myos.index(myo)==0 else 'left'
+
                 d = MyoData(arm_string, myo.acceleration.x, myo.acceleration.y, myo.acceleration.z, myo.orientation.roll, myo.orientation.pitch, myo.orientation.yaw) #myo.orientation.x, myo.orientation.y, myo.orientation.z, myo.orientation.w)
                 q.push(d)
 
@@ -110,17 +116,17 @@ def main():
 
                 if arm_string == 'right':
                     right_roll = max(2, min(10, abs(int(10 * myo.orientation.pitch / 3.14))))
-                    print("right: " + str(right_roll))
+                    # print("right: " + str(right_roll))
                     #msg.append(right_roll) #new_d.toString())
                     msg_data.append(right_roll)
                     # msg_data.append(right_roll)
-                    # if myo.pose == 'fist':
-                    #     msg.append(0.0)
-                    # else:
-                    #     msg.append(1.0)
+                    if myo.pose == 'fist':
+                        msg_data.append(0.0)
+                    else:
+                        msg_data.append(1.0)
                 else:
                     left_roll = max(200, min(10000, 60 + abs(int(15000 * myo.orientation.pitch / 3.14))))
-                    print("left: " + str(left_roll))
+                    # print("left: " + str(left_roll))
                     #msg.append(left_roll) #new_d.toString())
                     msg_data.append(left_roll)
                     # msg_data.append(left_roll)
@@ -131,8 +137,8 @@ def main():
 
                 target.write(new_d.toString() + "\n")
 
-
             msg.append(msg_data)
+            print(msg)
             client.send(msg)
 
             # client.send( OSCMessage("/user/1", [1.0, 2.0, 3.0 ] ) )
